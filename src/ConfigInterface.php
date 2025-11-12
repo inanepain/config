0937d54391b0b92010e4be83833a8f4d095df063
Philip Michael Raab <philip@cathedral.co.za>
2025-11-12 09:09:24 +0200

    config
    - config interface

---
+++ src/ConfigInterface.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * Inane: Config
+ *
+ * Configuration helpers.
+ *
+ * $Id$
+ * $Date$
+ *
+ * PHP version 8.4
+ *
+ * @author Philip Michael Raab<philip@cathedral.co.za>
+ * @package inanepain\config
+ * @category config
+ *
+ * @license UNLICENSE
+ * @license https://unlicense.org/UNLICENSE UNLICENSE
+ *
+ * _version_ $version
+ */
+
+/**
+ * Examples (controller & Service)
+ * Using a config key: myconfig
+ *
+ * config/autoload/myconfig.global.php
+ *
+ * return array(
+ *    'myconfig' => ['test' => true],
+ *);
+ *
+ * Module.php
+ *
+ * use Inane\Config\ConfigAwareInterface;
+ *
+ * public function getControllerConfig() {
+ *		return array(
+ *			'initializers' => array(
+ *				function ($instance, $sm) {
+ *					if ($instance instanceof ConfigAwareInterface) {
+ *						$locator = $sm->getServiceLocator();
+ *						$config = $locator->get('Config');
+ *						$instance->setConfig($config['myconfig']);
+ *					}
+ *				}));
+ *	}
+ *
+ * public function getServiceConfig() {
+ * 		return array(
+ * 			'initializers' => array(
+ * 				function ($instance, $sm) {
+ * 					if ($instance instanceof ConfigAwareInterface) {
+ * 						$config = $sm->get('Config');
+ * 						$instance->setConfig($config['myconfig']);
+ * 					}
+ * 				}));
+ * 	}
+ *
+ * IndexController.php
+ *
+ * use Inane\Config\ConfigAwareInterface;
+ *
+ * class IndexController extends AbstractActionController implements ConfigAwareInterface {
+ *
+ * protected $config;
+ *
+ * 	public function setConfig($config) {
+ * 		$this->config = $config;
+ * 	}
+ */
+
+declare(strict_types=1);
+
+namespace Inane\Config;
+
+use Inane\Stdlib\Array\OptionsInterface;
+
+/**
+ * ConfigInterface
+ *
+ * @version 0.1.0
+ */
+interface ConfigInterface extends OptionsInterface {
+	/**
+	 * configuration
+	 *
+	 * @param array|OptionsInterface $config configuration
+	 *
+	 * @return void
+	 */
+	public function getConfig(string $class): static;
+}
